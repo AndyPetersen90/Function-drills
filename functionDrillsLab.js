c02e10844029799d483aa670d1a60f41913c99e1
Andrew Petersen <fespetersen1990@gmail.com>
2022-04-18 16:15:27 -0600

    Problem 16 mostly solved

--- functionDrillsLab.js
+++ functionDrillsLab.js
@@ -305,19 +305,32 @@ function bigOrSmall(arr) {
   };
   return answers;
 };
-let arrayEvaluator = bigOrSmall(bigOrSmallArray);
-console.log(arrayEvaluator);
+// let arrayEvaluator = bigOrSmall(bigOrSmallArray);
+// console.log(arrayEvaluator);
 
 ////////////////// PROBLEM 16 ////////////////////
 let contestants = ['Katniss', 'Peeta', 'Fox-face', 'Glimmer', 'Cato', 'Rue', 'Thresh', 'Clove', 'Marvel']
 let loser = 'Glimmer'
 /*
-  Write a function that is called theEliminator, which takes in two arguments, contestants (which will each be an array of strings), and loser (which will be a string).
-  The function should loop over the array of contestant names. If the loser string appears in the array, splice it out. Return the new contestants array.
+  Write a function that is called theEliminator, which takes in two arguments, 
+  contestants (which will each be an array of strings), and loser (which will be a string).
+  The function should loop over the array of contestant names. If the loser string appears in the array, splice it out. 
+  Return the new contestants array.
 */
 
 //CODE HERE
 
+function theEliminator(contestants, loser){
+  for(i = 0; i < contestants.length; i++){
+    if(contestants[i] === loser){
+      contestants.splice(i, i - 1);
+    }
+  }
+  return contestants;
+}
+
+let newContestants = theEliminator(contestants, loser);
+console.log(newContestants);
 
 ////////////////// PROBLEM 17 ////////////////////
 let sampleString = "Hi, my name is Kylo."
