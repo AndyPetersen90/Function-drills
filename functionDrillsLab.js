500bead34363b1f91a829458e7682deda909c07c
Andrew Petersen <fespetersen1990@gmail.com>
2022-04-19 09:15:42 -0600

    Problem 22 Solved

--- functionDrillsLab.js
+++ functionDrillsLab.js
@@ -407,16 +407,49 @@ function whatEverIBelieveToBeAppropriate2(gold2){
   console.log(numberFrogs2); 
 };
 
-let totalFrogs2 = whatEverIBelieveToBeAppropriate2(32)
+// let totalFrogs2 = whatEverIBelieveToBeAppropriate2(32)
 
 ////////////////// PROBLEM 21 ////////////////////
-let sampleArray = [0,1,2,3,4,7,5,6,8,9]
+let sampleArray = [0,1,2,3,4,5,6,7,8,9]
 /*
-  Write a function that takes in an array of numbers as an argument. In the body of the function, write logic to determine if the array is in ascending order. The function should return true, if it is sorted in ascending order, false if it is not. Create a variable, `arrayIsAscending` and set it equal to your function invoked. Use the sample array to test this function.
+  Write a function that takes in an array of numbers as an argument. 
+  In the body of the function, write logic to determine if the array is in ascending order. 
+  The function should return true, if it is sorted in ascending order, false if it is not. 
+  Create a variable, `arrayIsAscending` and set it equal to your function invoked. 
+  Use the sample array to test this function.
 */
 
 //CODE HERE
 
+//Jeddy solution//
+
+function isArraySorted(arr){
+  let checker = arr[0];
+  for (i = 1; i < arr.length; i++){
+    if (arr[i] < checker){
+      return false;
+    } else {
+      checker = arr[i];
+    }
+  }
+  return true;
+
+}
+let arrayIsAscending = isArraySorted(sampleArray);
+console.log(arrayIsAscending);
+
+//My work//
+
+// function arr(numberArray){
+//   for (i = 0; i < numberArray.length; i++){
+//     if(numberArray[i] === i){
+//       console.log("True");
+//     } else {
+//       console.log("False");
+//     }
+//   }
+// }
+// let arrayIsAscending = arr(sampleArray);
 
 ////////////////// PROBLEM 22 ////////////////////
 
